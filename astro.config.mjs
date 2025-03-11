21cca67bb13ea33b1fa47685c9ab51160b1d53ba
M1dnightSUN <52265012+M1dnightSUN@users.noreply.github.com>
2025-03-12 04:15:16 +0900

    fix astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,7 +22,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
   site: "https://M1dnightSUN.github.io",
-  base: "/M1dnightSUN-Blog/",
+  base: "/MidnightSun-Blog",
   trailingSlash: "always",
   integrations: [
     tailwind(
