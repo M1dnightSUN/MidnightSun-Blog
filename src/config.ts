cbc798ade9504e85e684b0b42bd37cf22d9e6953
M1dnightSUN <52265012+M1dnightSUN@users.noreply.github.com>
2025-03-12 05:03:15 +0900

    change self info

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
   title: 'MidnightSun \'s Blog',
-  subtitle: 'Demo Site',
+  subtitle: 'Astro',
   lang: 'zh_CN',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
   themeColor: {
     hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
@@ -16,7 +16,7 @@ export const siteConfig: SiteConfig = {
   },
   banner: {
     enable: true,
-    src: 'assets/images/demo-banner.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+    src: 'assets/images/ds_campfire.jpg',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
     position: 'center',      // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
     credit: {
       enable: false,         // Display the credit text of the banner image
@@ -51,9 +51,9 @@ export const navBarConfig: NavBarConfig = {
 }
 
 export const profileConfig: ProfileConfig = {
-  avatar: 'assets/images/demo-avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: 'Lorem Ipsum',
-  bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  avatar: 'assets/images/kurage.jpg',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  name: 'MidnightSun',
+  bio: '一位普通的游戏爱好者',
   links: [
     {
       name: 'Twitter',
@@ -65,7 +65,7 @@ export const profileConfig: ProfileConfig = {
     {
       name: 'Steam',
       icon: 'fa6-brands:steam',
-      url: 'https://store.steampowered.com',
+      url: 'https://steamcommunity.com/profiles/76561198096225832/',
     },
     {
       name: 'GitHub',
--- src/content/spec/about.md
+++ src/content/spec/about.md
@@ -1,9 +1,9 @@
 # About
-This is the demo site for [Fuwari](https://github.com/saicaca/fuwari).
+一名普通的不死人。
 
-::github{repo="saicaca/fuwari"}
+::github{repo="M1dnightSUN/MidnightSun-Blog"}
 
 > ### Sources of images used in this site
-> - [Unsplash](https://unsplash.com/)
+<!-- > - [Unsplash](https://unsplash.com/)
 > - [星と少女](https://www.pixiv.net/artworks/108916539) by [Stella](https://www.pixiv.net/users/93273965)
-> - [Rabbit - v1.4 Showcase](https://civitai.com/posts/586908) by [Rabbit_YourMajesty](https://civitai.com/user/Rabbit_YourMajesty)
+> - [Rabbit - v1.4 Showcase](https://civitai.com/posts/586908) by [Rabbit_YourMajesty](https://civitai.com/user/Rabbit_YourMajesty) -->
